0510e0a72a03d981c3d649f8b897651ef8fcae9e
just_wasif <wasif.iitr.ansari@gmail.com>
2025-12-02 15:25:37 +0530

    feat:todo complete

--- redux_toolkit/src/components/Todos.jsx
+++ redux_toolkit/src/components/Todos.jsx
@@ -20,7 +20,7 @@ function Todos() {
       setEditingId(null);
       setEditingText('');
     }
-    const handleCanceEdit=()=>{
+    const handleCancelEdit=()=>{
       setEditingId(null);
       setEditingText('');
     };
@@ -28,70 +28,72 @@ function Todos() {
 
   return (
     <>
-      <div className="mt-8 text-xl font-semibold text-white">Todos</div>
-      <ul className="list-none mt-4 space-y-2">
-        {todos.map((todo) => (
-          <li
-            className="flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
-            key={todo.id}
-          >
-            {editingId === todo.id ? (
-              // 👇 Edit mode
-              <div className="flex w-full items-center gap-2">
-                <input
-                  type="text"
-                  className="flex-1 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  value={editingText}
-                  onChange={(e) => setEditingText(e.target.value)}
-                />
-                <button
-                  onClick={handleSaveEdit}
-                  className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 rounded text-sm"
-                >
-                  Save
-                </button>
-                <button
-                  onClick={handleCancelEdit}
-                  className="text-white bg-gray-500 border-0 py-1 px-4 focus:outline-none hover:bg-gray-600 rounded text-sm"
-                >
-                  Cancel
-                </button>
-              </div>
-            ) : (
-              <>
-                <div className="text-white flex-1">{todo.text}</div>
-                <div className="flex items-center gap-2">
+      <div >
+        <div className="mt-8 text-xl font-semibold text-green-700">Todos</div>
+        <ul className="list-none mt-4 space-y-2">
+          {todos.map((todo) => (
+            <li
+              className="flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+              key={todo.id}
+            >
+              {editingId === todo.id ? (
+                // 👇 Edit mode
+                <div className="flex w-full items-center gap-2">
+                  <input
+                    type="text"
+                    className="flex-1 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    value={editingText}
+                    onChange={(e) => setEditingText(e.target.value)}
+                  />
                   <button
-                    onClick={() => handleStartEdit(todo)}
-                    className="text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 rounded text-sm"
+                    onClick={handleSaveEdit}
+                    className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 rounded text-sm"
                   >
-                    Edit
+                    Save
                   </button>
                   <button
-                    onClick={() => dispatch(removeTodo(todo.id))}
-                    className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-sm flex items-center justify-center"
+                    onClick={handleCancelEdit}
+                    className="text-white bg-gray-500 border-0 py-1 px-4 focus:outline-none hover:bg-gray-600 rounded text-sm"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                      />
-                    </svg>
+                    Cancel
                   </button>
                 </div>
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
+              ) : (
+                <>
+                  <div className="text-white flex-1">{todo.text}</div>
+                  <div className="flex items-center gap-2">
+                    <button
+                      onClick={() => handleStartEdit(todo)}
+                      className="text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 rounded text-sm"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => dispatch(removeTodo(todo.id))}
+                      className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-sm flex items-center justify-center"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
