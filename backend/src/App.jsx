39134d08c94f3171abfc8eb02fc77a4faae69f08
just_wasif <wasif.iitr.ansari@gmail.com>
2025-12-04 11:40:52 +0530

    feat: initialize backend project with Vite and React
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application layout and components.
    - Developed App.jsx as the main application component, logging configuration values.
    - Implemented AuthService for handling user authentication with Appwrite.
    - Added React logo SVG to assets directory.
    - Configured environment variables in conf.js for Appwrite integration.
    - Established global styles in index.css for consistent theming and layout.
    - Set up main entry point in main.jsx to render the application.
    - Configured Vite with react plugin in vite.config.js for optimized builds.

---
+++ backend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+  console.log(conf.appwriteurl)
+
+  return (
+    <>
+      <h1>first backend</h1>
+    </>
+  )
+}
+
+export default App
---
+++ backend/src/appwrite/auth.js
@@ -0,0 +1,52 @@
+import conf from "../conf/conf";
+import {Client,Account,ID} from "appwrite"
+
+export class AuthService{
+    client=new Client();
+    
+    constructor(){
+        this.client
+            .setProject(conf.appwriteprojectid)
+            .setEndpoint(conf.appwriteurl);
+        this.account=new Account(this.client);
+    }
+    async creatAccount({email,password,name}){
+        try{
+            const user=await this.account.create({
+                userId:ID.unique(),
+                email:"@gmail.com",
+                password:"**********",
+                name:"."
+            });
+            console.log(user)
+        }catch (e){
+            console.error(e)
+        }
+    }
+
+    async login({email,password}){
+        try{
+            return await this.account.createEmailPasswordSession({email,password});
+        } catch (error){
+            throw error;
+        }
+    }
+    async getcurrentuser(){
+        try{
+            return await this.account.get()
+        }catch(error){
+            throw error;
+        }
+    }
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+        }catch(error){
+            throw error;
+        }
+
+    }
+}
+
+const authservice=new AuthService();
+export default authservice
